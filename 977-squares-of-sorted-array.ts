9b63b20bc554bc8f718d7e2639ec01764d4fff5a
Keyur R Garsondiya <keyurpatel8118@gmail.com>
2025-02-13 10:17:45 +0530

    Optimise Sorted Squares

--- 977-squares-of-sorted-array.ts
+++ 977-squares-of-sorted-array.ts
@@ -1,5 +1,23 @@
 function sortedSquares(nums: Array<number>): Array<number> {
-  nums.sort((a: number, b: number) => Math.abs(a) - Math.abs(b));
-  nums.forEach((num) => num * num);
-  return nums;
+  const n = nums.length;
+  const result: number[] = new Array(n);
+  let left = 0;
+  let right = n - 1;
+  let index = n - 1;
+
+  while (left <= right) {
+    const leftSquare = nums[left] * nums[left];
+    const rightSquare = nums[right] * nums[right];
+
+    if (leftSquare > rightSquare) {
+      result[index] = leftSquare;
+      left++;
+    } else {
+      result[index] = rightSquare;
+      right--;
+    }
+    index--;
+  }
+
+  return result;
 }
